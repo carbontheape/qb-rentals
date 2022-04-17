fd4a35f9c8c3cbfbf4a6408bcd1d30867ea01c79
plasma <83843797+plasmaFPS@users.noreply.github.com>
2022-04-17 01:30:23 -0700

    add config file

--- config.lua
+++ config.lua
@@ -1,27 +1,34 @@
 Config = {}
 
--- Vehicle Rentals 
-Config.vehicleped = `a_m_y_business_03`
-Config.vehlocation = { x = 109.9739, y = -1088.61, z = 28.302, w = 345.64 },
-Config.vehspawn = vector4(-1673.39, -3158.45, 13.99, 331.49)
+Config.FuelExport = 'LegacyFuel'
 
--- Aircraft Rentals
-Config.airped = `s_m_y_airworker`
-Config.vehlocation = { x = -1686.57, y = -3149.22, z = 12.99, w = 240.88 }
+Config.Locations = {
+    vehicle = {
+        coords = vector4(109.9739, -1088.61, 28.302, 345.64),
+        pedhash = `a_m_y_business_03`,
+        spawnpoint = vector4(111.4223, -1081.24, 29.192, 340.0),
+    },
 
--- Boat Rentals
-Config.boatped = `mp_m_boatstaff_01`
-Config.boatlocation = { x = -753.5, y = -1512.27, z = 4.02, w = 25.61 }
+    aircraft = {
+        coords = vector4(-1686.57, -3149.22, 12.99, 240.88),
+        pedhash = `s_m_y_airworker`,
+        spawnpoint = vector4(-1673.4, -3158.47, 13.99, 331.49),
+    },
 
-Config.blips = {
+    boat = {
+        coords = vector4(-753.5, -1512.27, 4.02, 25.61),
+        pedhash = `mp_m_boatstaff_01`,
+        spawnpoint = vector4(-794.95, -1506.27, 1.08, 107.79),
+    },
+}
+
+Config.Blips = {
     {title= Lang:t("info.land_veh"), colour= 50, id= 56, x= 111.0112, y= -1088.67, z= 29.302},
     {title= Lang:t("info.air_veh"), colour= 32, id= 578, x= -1673.39, y= -3158.45, z= 13.99},
     {title= Lang:t("info.sea_veh"), colour= 42, id= 410, x= -753.55, y= -1512.24, z= 5.02}, 
 }
 
-Config.fueltype = 'lj-fuel',
-
-Config.vehicles = {
+Config.Vehicles = {
     land = {
         [1] = {
             model = 'futo',
